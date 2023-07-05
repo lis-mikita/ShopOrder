07856dcf486fd84cfabcbd1208aeabb916407f6a
Mikita Lis <fox.nn.aa@gmail.com>
2023-07-05 17:09:12 +0200

    Implement extension method AddShopOrderDbContext for easy way to add Db in services to the container.

--- ShopOrder.Infrastructure.Data/ShopOrderDbContextExtensions.cs
+++ ShopOrder.Infrastructure.Data/ShopOrderDbContextExtensions.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ShopOrder.Infrastructure.Data
 {
-    internal class ShopOrderDbContextExtensions
+    public static class ShopOrderDbContextExtensions
     {
+        /// <summary>
+        /// Adds ShopOrderDbContext to the specified IServiceCollection. Uses the SqlServer database provider.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString">Set to override the default of "..".</param>
+        /// <returns>An IServiceCollection that can be used to add more services.</returns>
+        public static IServiceCollection AddShopOrderDbContext(
+            this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ShopOrderDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
     }
 }
