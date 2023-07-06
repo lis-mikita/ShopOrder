2815cbcfb81db65e044f25c84ab6acb43675166a
Mikita Lis <fox.nn.aa@gmail.com>
2023-07-06 11:16:15 +0200

    Implement interface of IOrderDetailRepository in OrderDetailRepository class.

--- ShopOrder.Infrastructure.Data/OrderDetails/OrderDetailRepository.cs
+++ ShopOrder.Infrastructure.Data/OrderDetails/OrderDetailRepository.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOrder.Domain.Core.Models.OrderDetails;
+using ShopOrder.Domain.Interfaces.OrderDetails;
 
 namespace ShopOrder.Infrastructure.Data.OrderDetails
 {
-    internal class OrderDetailRepository
+    public class OrderDetailRepository : IOrderDetailRepository
     {
+        private const int NO_CHANGES_SAVED_COUNT = 0;
+        private readonly ShopOrderDbContext _db;
+
+        public OrderDetailRepository(ShopOrderDbContext dbContext) => _db = dbContext;
+
+        public async Task<bool> CheckOrderExistsAsync(int id)
+        {
+            return await _db.Orders.AnyAsync(o => o.OrderId == id);
+        }
+
+        public async Task<OrderDetail?> CreateOrderDetailAsync(OrderDetail orderDetail)
+        {
+            await _db.OrderDetails.AddAsync(orderDetail);
+
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? orderDetail : throw new DbUpdateException();
+        }
+
+        public async Task<bool> DeleteOrderDetailAsync(int id)
+        {
+            var orderDetail = await _db.OrderDetails.FindAsync(id);
+            if (orderDetail is null)
+            {
+                return false;
+            }
+
+            _db.OrderDetails.Remove(orderDetail);
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? true : throw new DbUpdateException();
+        }
+
+        public async Task<OrderDetail?> GetOrderDetailAsync(int id)
+        {
+            return await _db.OrderDetails.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<OrderDetail>> GetOrderDetailsAsync()
+        {
+            return await _db.OrderDetails.ToListAsync();
+        }
+
+        public async Task<OrderDetail?> UpdateOrderDetailAsync(int id, OrderDetail orderDetail)
+        {
+            var existingOrderDetail = await _db.OrderDetails.FindAsync(id);
+            if (existingOrderDetail is null)
+            {
+                return null;
+            }
+
+            existingOrderDetail.ProductName = orderDetail.ProductName;
+            existingOrderDetail.Price = orderDetail.Price;
+            existingOrderDetail.Quantity = orderDetail.Quantity;
+
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? existingOrderDetail : throw new DbUpdateException();
+        }
     }
 }
