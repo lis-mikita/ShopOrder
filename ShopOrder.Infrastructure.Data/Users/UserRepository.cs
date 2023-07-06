52520a5efb96225300387696b3551cf1a3603698
Mikita Lis <fox.nn.aa@gmail.com>
2023-07-06 10:44:29 +0200

    Implement interface of IUserRepository in UserRepository class.

--- ShopOrder.Infrastructure.Data/Users/UserRepository.cs
+++ ShopOrder.Infrastructure.Data/Users/UserRepository.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOrder.Domain.Core.Models.Users;
+using ShopOrder.Domain.Interfaces.Users;
 
 namespace ShopOrder.Infrastructure.Data.Users
 {
-    internal class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private const int NO_CHANGES_SAVED_COUNT = 0;
+        private readonly ShopOrderDbContext _db;
+
+        public UserRepository(ShopOrderDbContext dbContext) => _db = dbContext;
+
+        public async Task<bool> CheckIfSameEmailExistsAsync(string email)
+        {
+            return await _db.Users.AnyAsync(u => u.Email != null && u.Email.ToUpper() == email.ToUpper());
+        }
+
+        public async Task<User?> CreateUserAsync(User user)
+        {
+            await _db.Users.AddAsync(user);
+
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? user : throw new DbUpdateException();
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+            if (user is null)
+            {
+                return false;
+            }
+
+            _db.Users.Remove(user);
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? true : throw new DbUpdateException();
+        }
+
+        public async Task<User?> UpdateUserAsync(int id, User user)
+        {
+            var existingUser = await _db.Users.FindAsync(id);
+            if (existingUser is null)
+            {
+                return null;
+            }
+
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+            existingUser.Password = user.Password;
+            existingUser.Orders = user.Orders;
+            existingUser.RegistrationDate = user.RegistrationDate;
+
+            return (await _db.SaveChangesAsync()) > NO_CHANGES_SAVED_COUNT ? existingUser : throw new DbUpdateException();
+        }
+
+        public async Task<User?> GetUserAsync(int id)
+        {
+            return await _db.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _db.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetUserWithOrdersAsync(int id)
+        {
+            return await _db.Users.Include(u => u.Orders).FirstAsync(u => u.UserId == id);
+        }
     }
 }
