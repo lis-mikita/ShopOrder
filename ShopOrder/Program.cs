22c55ef06ba7c1818ab4a7d7d426584a58780e00
Mikita Lis <fox.nn.aa@gmail.com>
2023-07-07 09:12:46 +0200

    Add logging (Serilog) warning and error to file in controllers. Created class (ServiceRegistration) for register DI.

--- ShopOrder/Program.cs
+++ ShopOrder/Program.cs
@@ -1,11 +1,9 @@
+using ShopOrder.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.RegisterDI(builder.Configuration);
 
 var app = builder.Build();
 
--- ShopOrder/Utilities/ServiceRegistration.cs
+++ ShopOrder/Utilities/ServiceRegistration.cs
@@ -1,6 +1,46 @@
-﻿namespace ShopOrder.Utilities
+﻿using ShopOrder.Domain.Interfaces.Users;
+using ShopOrder.Infrastructure.Business.Users;
+using ShopOrder.Infrastructure.Data.Users;
+using ShopOrder.Services.Interfaces.Users;
+using ShopOrder.Infrastructure.Data;
+using ShopOrder.Infrastructure.Business.Orders;
+using ShopOrder.Services.Interfaces.Orders;
+using ShopOrder.Infrastructure.Data.Orders;
+using ShopOrder.Domain.Interfaces.Orders;
+using ShopOrder.Services.Interfaces.OrderDetails;
+using ShopOrder.Domain.Interfaces.OrderDetails;
+using ShopOrder.Infrastructure.Data.OrderDetails;
+using ShopOrder.Infrastructure.Business.OrderDetails;
+
+namespace ShopOrder.Utilities
 {
-    public class ServiceRegistration
+    public static class ServiceRegistration
     {
+        public static IServiceCollection RegisterDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Db
+            services.AddShopOrderDbContext(configuration.GetConnectionString("ShopConnection") ?? "DefaultConnectionString");
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IOrderDetailService, OrderDetailService>();
+
+            // Swagger
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            // Logger
+            services.AddLogging(builder =>
+            {
+                builder.AddFile(configuration.GetSection("Logging"));
+            });
+
+            return services;
+        }
     }
 }
