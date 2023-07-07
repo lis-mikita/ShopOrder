deb97b864ca07615e36b203bffd72b0f77e92938
Mikita Lis <fox.nn.aa@gmail.com>
2023-07-07 09:39:29 +0200

    Add templates for unit tests.

---
+++ ShopOrder.Infrastructure.UnitTests/Orders/DeleteOrderAsync.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOrder.Infrastructure.UnitTests.Orders
+{
+    internal class DeleteOrderAsync
+    {
+    }
+}
